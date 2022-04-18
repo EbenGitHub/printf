b6ce61e0fa23b23a2de4b310863dd2b10dce58b9
Abenezer Eshetie <abenezergoo@gmail.com>
2022-04-18 07:57:49 +0300

    This is test

--- _printf.c
+++ _printf.c
@@ -4,52 +4,14 @@
 #include <string.h>
 #include <limits.h>
 
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-
-#include <unistd.h>
-int _putchar(char c)
-{
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
-}
-
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
 	const char *p;
 	va_list arguments;
-	flags_t flags = {0, 0, 0};
-
 	register int count = 0;
 
 	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
+
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -57,20 +19,21 @@ int _printf(const char *format, ...)
 			p++;
 			if (*p == '%')
 			{
-				count += _putchar('%');
+				printf("%%");
+				count += 1;
 				continue;
 			}
-			while (0)
-				p++;
-//			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
+			else
+		    {
+		        printf("%i", va_arg(arguments, int));
+		 	    count += 1;   
+		    }
 		}
 		else
-			count += _putchar(*p);
+		printf("%c", *p);
+	    count += 1;
+		
 	}
-	_putchar(-1);
 	va_end(arguments);
 	return (count);
 }
