4b428dee6175c68bd109004d01c1ff6cda561804
Abenezer Eshetie <abenezergoo@gmail.com>
2022-04-19 19:39:15 +0300

    printing number bug fixed

--- numprint.c
+++ numprint.c
@@ -1,37 +1,38 @@
 #include "main.h"
 
 /**
- * print_int - will prints an integer
+ * print_int - prints an integer
  * @l: va_list of arguments from _printf
  * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
 int print_int(va_list l, flags_t *f)
 {
 	int n = va_arg(l, int);
-	int resi = count_digit(n);
+	int res = count_digit(n);
 
 	if (f->space == 1 && f->plus == 0 && n >= 0)
-		resi += _putchar(' ');
+		res += _putchar(' ');
 	if (f->plus == 1 && n >= 0)
-		resi += _putchar('+');
+		res += _putchar('+');
 	if (n <= 0)
-		resi++;
+		res++;
 	print_number(n);
-	return (resi);
+	return (res);
 }
 
 /**
  * print_unsigned - prints an unsigned integer
  * @l: va_list of arguments from _printf
- * @f: pointer to the structure of flags determining
- * if a flag is passed to _printf func
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
  * Return: number of char printed
  */
 int print_unsigned(va_list l, flags_t *f)
 {
-	unsigned int un_i = va_arg(l, unsigned int);
-	char *str = convert(un_i, 10, 0);
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
 	(void)f;
 	return (_puts(str));
@@ -39,8 +40,8 @@ int print_unsigned(va_list l, flags_t *f)
 
 /**
  * print_number - helper function that loops through
- * an integer and prints all its digit numbers
- * @n: integer numbers to be printed
+ * an integer and prints all its digits
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
